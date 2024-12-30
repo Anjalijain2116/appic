fb3f81d3d2e4c717cc99e5edb44c735b0a9a2963
Anjalijain2116 <87710908+Anjalijain2116@users.noreply.github.com>
2024-12-30 13:00:26 +0530

    Delete angular-task directory

--- angular-task/src/app/core/directives/attribute.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AttributeDirective } from './attribute.directive';
-
-describe('AttributeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AttributeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
